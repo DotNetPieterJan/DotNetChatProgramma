8f5513a3509bfbda9d15911a23094b179f67ff38
PolM <PolM@PC216-13>
2017-02-28 15:53:39 +0100

    Chatroom update1

--- DBLib/Models/Chatroom.cs
+++ DBLib/Models/Chatroom.cs
@@ -8,6 +8,42 @@ namespace DBLib.Models
 {
     public class Chatroom
     {
-        public List<Member>
+        public Member Creator { get; private set; }
+        public List<Member> Admins { get; private set; }
+        public List<Guest> MembersInChatroom { get; private set; }
+        public List<Message> Messages { get; private set; }
+        
+        public Chatroom(Member creator)
+        {
+            Creator = creator;
+            Admins = new List<Member>();
+            MembersInChatroom = new List<Guest>();
+            Messages = new List<Message>();
+        }
+
+        public void AddAdmin(Member member)
+        {
+            Admins.Add(member);
+        }
+        public void RemoveAdmin(Member member)
+        {
+            Admins.Remove(member);
+        }
+        public void AddMessage(Message message)
+        {
+            Messages.Add(message);
+        }
+        public void RemoveMessage(Message message)
+        {
+            Messages.Remove(message);
+        }
+        public void AddMemberToChatroom(Guest guest)
+        {
+            MembersInChatroom.Add(guest);
+        }
+        public void RemoveMemberFromChatroom(Guest guest)
+        {
+            MembersInChatroom.Remove(guest);
+        }
     }
 }
