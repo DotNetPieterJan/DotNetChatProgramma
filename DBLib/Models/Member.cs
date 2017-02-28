6808cf956fbbeb712c96a84b71a09c1a1f4b0d90
sveng <svengysels@gmail.com>
2017-02-28 15:29:06 +0100

    Member aangemaakt.

---
+++ DBLib/Models/Member.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLib.Models
+{
+    class Member : Guest
+    {
+        public string Password { get; set; }
+        public bool IsOnline { get; set; }
+        public Member(string username, string password, bool isOnline)
+        {
+
+        }
+        public void MakeRoom(Member member)
+        {
+
+        }
+        public void ChangePassword(Member member)
+        {
+
+        }
+        public bool FindUser(Member member)
+        {
+            return false;
+        }
+        public void AdminAddsMember(Member member)
+        {
+
+        }
+        public void AdminKicksMember(Member member)
+        {
+
+        }
+        public void AdminDeletesMessage(Message message)
+        {
+
+        }
+        public void CreatorPromotesMember(Member member)
+        {
+
+        }
+        public void CreatorDemotesMember(Member member)
+        {
+
+        }
+
+
+
+    }
+}
