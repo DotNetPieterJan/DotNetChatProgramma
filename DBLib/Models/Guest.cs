8e89032b27aed9372236e245dc1fd26834d94064
DotNetPieterJan <pieterjan105@gmail.com>
2017-02-28 14:53:02 +0100

    skeleton build Guest

---
+++ DBLib/Models/Guest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLib.Models
+{
+    class Guest
+    {
+        public int CurrentChatroom { get; set; } //ID
+        public string Username { get; set; } //Guest : random generated username
+        public void Register() //converts to Member
+        {
+
+        }
+        public void SendMessage(int currentChatroom) //send message to CurrentChatroom
+        {
+            if (currentChatroom == CurrentChatroom)//no hax
+            {
+
+            }
+        }
+    }
+}
